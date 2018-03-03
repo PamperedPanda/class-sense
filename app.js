44b98a0541d5ea50a60d26b45c5688146a326545
Quin Carnahan <qcarnahanhome@gmail.com>
2018-03-03 15:23:49 -0800

    first working version of website

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.set("view engine", "ejs");
 app.get("/", function(req, res){
     res.render("index");
 });
-app.get(req, res)
 
-app.listen(5000);
-console.log('server is running')
+app.listen(5000, function(){
+    console.log("Server started!");
+});
