a9a683b74308ae030c3e4e8ca324481b9042274a
Quin Carnahan <qcarnahanhome@gmail.com>
2018-03-03 15:55:05 -0800

    routes added in app.js

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 var express = require('express');
 var app = express();
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
     res.render("index");
+});
+
+app.post("/location", function(req, res){
+	res.send
 });
 
 app.listen(5000, function(){
