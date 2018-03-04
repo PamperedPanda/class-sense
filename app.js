763af6e770a8f7beb8b996ac0efe05bc1ccaa8dd
Quin Carnahan <qcarnahanhome@gmail.com>
2018-03-03 19:04:45 -0800

    Newest Version

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require("body-parser");
 var request = require("request");
+var queryString = require("querystring");
 
 var longitude = 0;
 var latitude = 0;
@@ -12,19 +13,23 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
+	var lat = queryString.parse('lat');
+    var ltd = queryString.parse('ltd');
     res.render("home");
 
 });
 
+app.get("/test", function(req, res){
+	res.render("test");
+});
+
 app.post("/location", function(req, res){
 	var address = req.body.address;
-
 	request("https://maps.googleapis.com/maps/api/geocode/json?address=" + address, function(err, res, body){
 		var longitude = req.param('lat');
 		var latitude = req.param('lng');
-	})
-	res.send(longitude + " " + latitude);
-	res.redirect("/", {lat: latitude, ltd:latitude});
+	});
+	res.redirect("/?lat=" + latitude + "&ltd=" + latitude);
 });
 
 app.listen(5000, function(){
---
+++ views/locationHandler.js
@@ -0,0 +1,1 @@
+geocoder = new google.maps.Geocoder();
